fc3759248d80ab795b804be77aff876a656d882b
Ncr2001 <chandanansr2001@gmail.com>
2023-12-04 20:54:32 +0530

    switch case

---
+++ src/vit/xyz/emp/control3.java
@@ -0,0 +1,56 @@
+package vit.xyz.emp;
+
+public class control3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int date = 5;
+	      String day;
+	 
+	        // Switch statement with int data type
+	        switch (date) {
+	 
+	        // Case
+	        case 1:
+	            day = "Monday";
+	            break;
+	 
+	        // Case
+	        case 2:
+	            day = "Tuesday";
+	            break;
+	 
+	            // Case
+	        case 3:
+	            day = "Wednesday";
+	            break;
+	 
+	            // Case
+	        case 4:
+	            day = "Thursday";
+	            break;
+	 
+	        // Case
+	        case 5:
+	            day = "Friday";
+	            break;
+	 
+	            // Case
+	        case 6:
+	            day = "Saturday";
+	            break;
+	 
+	            // Case
+	        case 7:
+	            day = "Sunday";
+	            break;
+	 
+	        // Default case
+	        default:
+	            day = "Invalid date";
+	        }
+	        System.out.println(day);
+	    
+	}
+
+}
