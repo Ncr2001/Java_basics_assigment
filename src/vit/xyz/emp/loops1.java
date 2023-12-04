d6edfb409b71158badeddef60e7675840ef8d301
Ncr2001 <chandanansr2001@gmail.com>
2023-12-04 21:08:41 +0530

    while loop

---
+++ src/vit/xyz/emp/loops1.java
@@ -0,0 +1,16 @@
+package vit.xyz.emp;
+
+public class loops1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i = 0;    
+		System.out.println("Printing the list of first 10 even numbers \n");    
+		while(i<=10) {    
+		System.out.println(i);  
+		i = i + 2;
+		}    
+		
+	}
+
+}
