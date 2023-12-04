16f9340b955d9620bd2da1fba419b8e14ac984d8
Ncr2001 <chandanansr2001@gmail.com>
2023-12-04 15:04:11 +0530

    Arthmatic operatios

---
+++ src/vit/xyz/emp/Arthmatic.java
@@ -0,0 +1,17 @@
+package vit.xyz.emp;
+
+public class Arthmatic {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 22;
+		int b = 20;
+		System.out.println("A+B="+(a+b));
+		System.out.println("A-B=" +(a-b));
+		System.out.println("A*B="+(a*b));
+		System.out.println("A/B=" +(a/b));
+		
+
+	}
+
+}
