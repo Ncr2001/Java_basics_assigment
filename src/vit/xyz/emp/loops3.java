23caa4f5a02197a976b9f999c7b4e3b11a041edd
Ncr2001 <chandanansr2001@gmail.com>
2023-12-04 22:36:37 +0530

    for loop

---
+++ src/vit/xyz/emp/loops3.java
@@ -0,0 +1,19 @@
+package vit.xyz.emp;
+
+public class loops3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i<= 2; i++) {  
+			  
+			for (int j = i; j<=5; j++) {  
+			  
+			if(j == 4) {  
+			continue;  
+			}  
+			System.out.println(j);  
+			}
+		}
+	}
+}
+	
